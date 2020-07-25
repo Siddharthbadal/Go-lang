3a5e0b7ee5413b81f52ecb7bb1c10a3d90b760eb
Sidharth badal <siddharthabadal.2015@gmail.com>
2020-07-25 15:24:19 +0530

    First Go Program
    Printed out a gopher in Go!

---
+++ gopher.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	t "time"
+)
+
+// func main () {
+//   fmt.Println(" __     _")
+//   fmt.Println("0'')}___//")
+//   fmt.Println("`_/      )")
+//   fmt.Println("(_(_/-(_/")
+// }
+
+func main() {
+	fmt.Println("    `.-::::::-.`    ")
+	fmt.Println(".:-::::::::::::::-:.")
+	fmt.Println("`_:::    ::    :::_`")
+	fmt.Println(" .:( ^   :: ^   ):. ")
+	fmt.Println(" `:::   (..)   :::. ")
+	fmt.Println(" `:::::::UU:::::::` ")
+	fmt.Println(" .::::::::::::::::. ")
+	fmt.Println(" O::::::::::::::::O ")
+	fmt.Println(" -::::::::::::::::- ")
+	fmt.Println(" `::::::::::::::::` ")
+	fmt.Println("  .::::::::::::::.  ")
+	fmt.Println("    oO:::::::Oo     ")
+	fmt.Println(t.Now())
+}
